2ffb5c9c9d20da79a6fb89a508543ed952ddb32c
lauf8 <lucasafonsodesouza@gmail.com>
2023-08-23 14:06:59 -0300

    home criado

--- atalia/settings.py
+++ atalia/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'atalia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'apss/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
